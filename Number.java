41bf73ff499d6f83d1c544aaf7b635311d62d618
Luís Farias <luisgmfarias@gmail.com>
2022-03-29 13:12:59 -0300

    add Number object getter and setters

---
+++ Number.java
@@ -0,0 +1,15 @@
+public class Number {
+    private int num;
+
+    public Number() {
+        this.num = 0;
+    }
+
+    public void setNumber(int num) {
+        this.num = num;
+    }
+
+    public int getNumber() {
+        return this.num;
+    }
+}
